23cccce97e113f2177e0e61e5f49d97d726a6af2
Vladislav Bobyrev <perets1995@gmail.com>
2024-02-05 21:22:35 +0300

    init rules

---
+++ index.ts
@@ -0,0 +1,17 @@
+// Boolean
+let isCompleted: boolean = false
+
+//Number
+const binary: number = 0b0101
+
+//String
+const userName: string = 'Vladislav'
+
+// Null and Undefined
+const u: undefined = undefined
+const n: null = null
+
+//for function result
+const getUser = ():void => {
+  console.log('Vladislav')
+}
